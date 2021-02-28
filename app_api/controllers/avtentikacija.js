048bbfa2f999db3ddb9fb7f4c04373155e080a23
parch99 <sskined@gmail.com>
2021-02-28 14:52:44 +0100

    new build

--- app_api/controllers/avtentikacija.js
+++ app_api/controllers/avtentikacija.js
@@ -71,7 +71,7 @@ const sendRecoveryEmail = (req, res) => {
         transporter.sendMail({
           to:user.elektronskiNaslov,
           from:'"MySeat" <stefi__stefan@hotmail.com>',
-          subject:"Password Reset", // CHANGE URL ON DEPLOY https://myseat-sp-2020-2021.herokuapp.com
+          subject:"Password Reset", // CHANGE URL ON DEPLOY
           html:`<h3>You requested a password reset on MySeat</h3> 
           <h3>Click on this <a href="https://myseat-sp-2020-2021.herokuapp.com/reset/${resettoken}">link</a> to proceed, If this
           was not requested by you please take action and change your password</h3>
